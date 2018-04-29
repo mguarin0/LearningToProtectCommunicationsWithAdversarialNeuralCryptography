1ee10be654d98d56b1d72695c2d295ccf2698261
mguarin0 <michael.guarino0101@gmail.com>
2018-04-29 19:39:04 -0400

    updated model architecture to better meets paper spec, fixed input data problem currently bound between (-1,1) as specified in paper, added reconstruction error for bob and eve, now must create total error

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.optim import Adam
 from torch.autograd import Variable
+import logging
 import argparse
 import os
 
@@ -39,36 +40,91 @@ def get_args():
     return args
 # end
 
-def train(epochs=10, num_batches_per_epoch=1000, batch_size=30, learning_rate=0.0008, n=16):
+"""
+def get_logger(log_dir):
+
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+    if not os.path.exists(os.path.join(log_dir, "train")):
+        os.mkdir(os.path.join(log_dir, "train"))
+
+    current_Time = str(datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s:%(name)s:%(message)s")
+    fileHandler = logging.FileHandler(os.path.join(log_dir, "train", "train_xception_%s.log"%current_Time))
+    fileHandler.setFormatter(formatter)
+
+    logger.addHandler(fileHandler)
+
+    return logger
+# end
+"""
+
+def train(gpu_available, epochs=200,
+          num_batches_per_epoch=1000,
+          batch_size=512,
+          learning_rate=0.0008,
+          n=16,
+          show_every_n_steps=500):
+
     print("train")
 
+    # define networks
+    alice = CryptoNN(D_in=(n*2), H=(n*2))
+    bob = CryptoNN(D_in=(n*2), H=(n*2))
+    eve = CryptoNN(D_in=n, H=(n*2))
+
+    if gpu_available:
+        alice.cuda()
+        bob.cuda()
+        eve.cuda()
+
+    # aggregate errors
     bob_training_errors = []
     eve_training_errors = []
 
-    alice = CryptoNN(D_in=(n*2), H=n)
-    bob = CryptoNN(D_in=(n*2), H=n)
-    eve = CryptoNN(D_in=(n*2), H=n)
-
+    # define optimizers
     optimizer_alice_bob = Adam(params=list(alice.parameters()) + list(bob.parameters()), lr=learning_rate)
     optimizer_eve = Adam(params=eve.parameters(), lr=learning_rate)
 
+    # define losses 
+    reconstruction_error_bob = nn.L1Loss()
+    reconstruction_error_eve = nn.L1Loss()
+
+    # training loop
     for epoch in range(epochs):
         bob_decrypt_error = 1.0
         eve_decrypt_error = 1.0
 
         for network in ["alice_bob", "eve"]:
 
-            if network == "eve":
-                num_batches_per_epoch *= 2
+            #if network == "eve":
+            #    num_batches_per_epoch *= 2
 
-            for i in range(num_batches_per_epoch):
+            for step in range(num_batches_per_epoch):
 
-                p, k = generate_data(batch_size=batch_size, n=n)
+                p, k = generate_data(gpu_available=gpu_available, batch_size=batch_size, n=n)
+
                 alice_c = alice.forward(torch.cat((p, k), 1).float())
-                bob_p = bob.forward(torch.cat((alice_c.float(), k.float()), 1).float())
+                bob_p = bob.forward(torch.cat((alice_c, k), 1).float())
 
-                #loss_alice_bob = 
-
+                if network == "alice_bob":
+                    error_bob = reconstruction_error_bob(input=bob_p, target=p)
+                    optimizer_alice_bob.zero_grad()
+                    error_bob.backward()
+                    optimizer_alice_bob.step()
+                
+                if step % show_every_n_steps == 0:
+                    print("Epoch:{} || Training:{} || Step:{} of {} || Loss:{}".format(epoch,
+                                                                                       network,
+                                                                                       step,
+                                                                                       num_batches_per_epoch,
+                                                                                       error_bob))
+                if step == num_batches_per_epoch-1:
+                    print("p[0]: {} \n".format(p[0]))
+                    print("alice_c[0]: {} \n".format(alice_c[0]))
+                    print("bob_p[0]: {} \n".format(bob_p[0]))
 # end
 
 def validate():
@@ -88,7 +144,7 @@ def main():
         gpu_available = False
 
     if args.run_type == "train":
-        train()
+        train(gpu_available=gpu_available)
 # end
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -6,39 +6,32 @@ class CryptoNN(torch.nn.Module):
         super(CryptoNN, self).__init__()
         self.fc_layer = torch.nn.Linear(D_in, H)
         self.conv1 = torch.nn.Conv1d(in_channels=1,
-                                     out_channels=3,
-                                     kernel_size=1,
-                                     #kernel_size=(4, 1, 2),
-                                     stride=1)
-        self.conv2 = torch.nn.Conv1d(in_channels=3,
+                                     out_channels=2,
+                                     kernel_size=4,
+                                     stride=1,
+                                     padding=2)
+        self.conv2 = torch.nn.Conv1d(in_channels=2,
                                      out_channels=4,
-                                     kernel_size=1,
-                                     #kernel_size=(2, 2, 4),
-                                     stride=1)
+                                     kernel_size=2,
+                                     stride=2)
         self.conv3 = torch.nn.Conv1d(in_channels=4,
                                      out_channels=4,
                                      kernel_size=1,
-                                     #kernel_size=(1, 4, 4),
                                      stride=1)
         self.conv4 = torch.nn.Conv1d(in_channels=4,
                                      out_channels=1,
                                      kernel_size=1,
-                                     #kernel_size=(1, 4, 1),
                                      stride=1)
     # end
 
     def forward(self, x):
-        #filter_var = torch.autograd.Variable(torch.zeros(4,16))
-        #x = F.conv1d(input=x, weight=filter_var)
-        print("x: {}".format(x.shape))
 
         x = x[None, :, :].transpose(0, 1)
-        #print("x: {}".format(x.shape))
 
         x = F.sigmoid(self.fc_layer(x))
         #print("x fc_layer: {}".format(x.shape))
 
-        x = self.conv1(x)
+        x = F.sigmoid(self.conv1(x))
         #print("x conv1: {}".format(x.shape))
 
         x = F.sigmoid(self.conv2(x))
