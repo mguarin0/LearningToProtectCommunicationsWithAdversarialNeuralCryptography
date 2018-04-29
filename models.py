61dbaa6b962b88d0cdcf4c3fb5ed7969861bc890
mguarin0 <michael.guarino0101@gmail.com>
2018-04-28 23:52:10 -0400

    convolution working but need to get multiple filters working

--- models.py
+++ models.py
@@ -7,26 +7,29 @@ class CryptoNN(torch.nn.Module):
         self.fc_layer = torch.nn.Linear(D_in, H)
         self.conv1 = torch.nn.Conv1d(in_channels=1,
                                      out_channels=3,
-                                     kernel_size=(4, 1, 2),
+                                     kernel_size=4,
+                                     #kernel_size=(4, 1, 2),
                                      stride=1)
-        """
-        self.conv2 = torch.nn.Conv1d(in_channels=2,
+        self.conv2 = torch.nn.Conv1d(in_channels=3,
                                      out_channels=4,
-                                     kernel_size=(2, 2, 4),
+                                     kernel_size=2,
+                                     #kernel_size=(2, 2, 4),
                                      stride=2)
         self.conv3 = torch.nn.Conv1d(in_channels=4,
                                      out_channels=4,
-                                     kernel_size=(1, 4, 4),
+                                     kernel_size=4,
+                                     #kernel_size=(1, 4, 4),
                                      stride=1)
         self.conv4 = torch.nn.Conv1d(in_channels=4,
                                      out_channels=1,
-                                     kernel_size=(1, 4, 1),
+                                     kernel_size=1,
+                                     #kernel_size=(1, 4, 1),
                                      stride=1)
-        """
     # end
 
     def forward(self, x):
         #filter_var = torch.autograd.Variable(torch.zeros(4,16))
+        #x = F.conv1d(input=x, weight=filter_var)
         print("x: {}".format(x.shape))
 
         x = x[None, :, :].transpose(0, 1)
@@ -35,17 +38,14 @@ class CryptoNN(torch.nn.Module):
         x = F.sigmoid(self.fc_layer(x))
         print("x: {}".format(x.shape))
         
-        #x = F.conv1d(input=x, weight=filter_var)
-        
         x = self.conv1(x)
         print("x: {}".format(x.shape))
-        """
-        x = F.sigmoid(self.conv1(x))
-        print("x: {}".format(x.shape))
         x = F.sigmoid(self.conv2(x))
+        print("x: {}".format(x.shape))
         x = F.sigmoid(self.conv3(x))
+        print("x: {}".format(x.shape))
         x = F.tanh(self.conv4(x))
-        """
+        print("x: {}".format(x.shape))
         return x
     # end
 # end
